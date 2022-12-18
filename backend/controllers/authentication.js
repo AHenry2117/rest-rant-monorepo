89abc19c0aa5715c7fb2ddf3fa6c7692e2d30dd5
AHenry2117 <aehenry2117@gmail.com>
2022-12-18 11:45:34 -0800

    added jwt

--- backend/controllers/authentication.js
+++ backend/controllers/authentication.js
@@ -1,14 +1,35 @@
 const router = require('express').Router()
 const db = require('../models');
 const bcrypt = require('bcrypt');
+const jwt = require('json-web-token')
 
-const { User } = db;
+const { user } = db;
 
-  
+router.get('/profile', async (req, res) => {
+    try {
+        const[authenticationMethod, token] = req.headers.authorization.split('')
+
+        if(authenticationMethod === 'Bearer'){
+            const result = await jwt.decode(process.env.JWT_SECRET, token);
+
+            const { id } = result.value
+            let user = await user.findOne({
+                where: {
+                    userId: id
+                }
+            });
+        }    
+        res.json(user);
+
+    } catch (e) {
+        res.json(null)
+    }
+})
+ 
   
 router.post('/', async (req, res) => {
     
-    let user = await User.findOne({
+    let user = await user.findOne({
         where: { email: req.body.email }
     })
 
@@ -17,7 +38,8 @@ router.post('/', async (req, res) => {
             message: `Could not find a user with the provided username and password` 
         })
     } else {
-        res.json({ user })
+        const { value } = await jwt.encode(process.env.JWT_SECRET, {id: user.userId});
+        res.json({ user, token: value });
     }
 })
   
